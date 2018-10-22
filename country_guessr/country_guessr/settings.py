7c785de8571298b43e7c2681c3c4374b30b0bdc9
softengineer123 <engineerwork@protonmail.com>
2018-10-22 19:58:44 +0100

    add rest_framework andspecify paginated requests

--- country_guessr/country_guessr/settings.py
+++ country_guessr/country_guessr/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'world',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
