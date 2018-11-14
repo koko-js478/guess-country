2e50463b567b93879eaf0a3ec9f482cff5143a0f
softengineer123 <engineerwork@protonmail.com>
2018-11-14 16:25:36 +0000

    add geodjango geojson serialization

--- country_guessr/django_rest/api/serializers.py
+++ country_guessr/django_rest/api/serializers.py
@@ -1,16 +1,18 @@
 from world.models import WorldBorder
-from rest_framework import serializers
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
-class WorldRandomCountrySerializer(serializers.HyperlinkedModelSerializer):
+class WorldRandomCountrySerializer(GeoFeatureModelSerializer):
     """Serializes a random world country"""
     class Meta:
         model = WorldBorder
         fields = ('name', 'name', 'lon', 'lat', 'mpoly')
+        geo_field = "mpoly"
 
 
-class WorldCountriesSerializer(serializers.HyperlinkedModelSerializer):
+class WorldCountriesSerializer(GeoFeatureModelSerializer):
     """Serializes all world countries"""
     class Meta:
         model = WorldBorder
         fields = ('name', 'name', 'lon', 'lat', 'mpoly')
+        geo_field = "mpoly"
--- country_guessr/django_rest/country_guessr/settings.py
+++ country_guessr/django_rest/country_guessr/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'world',
     'api',
     'rest_framework',
+    'rest_framework_gis',
 ]
 
 MIDDLEWARE = [
