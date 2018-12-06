889b6c83062775909998679ebf3ba04143a0cc09
softengineer123 <engineerwork@protonmail.com>
2018-12-06 21:47:24 +0000

    add username and password to LoginPage

--- country_guessr/ionic_app/src/pages/login/login.ts
+++ country_guessr/ionic_app/src/pages/login/login.ts
@@ -15,6 +15,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  username: string = "";
+  password: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
