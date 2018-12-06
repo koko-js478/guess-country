98899961e8c013a081ce6315d1ce8dd2c35e9302
softengineer123 <engineerwork@protonmail.com>
2018-12-06 14:14:10 +0000

    create new user

--- country_guessr/django_rest/api/models.py
+++ country_guessr/django_rest/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.core.validators import MaxValueValidator
+
+
+class Player(AbstractUser):
+    points = models.PositiveIntegerField(validators=[MaxValueValidator(1000000)], default=0)
