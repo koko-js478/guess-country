6d1c29e8eb51ef7c4ad270545dc96953aee01021
softengineer123 <engineerwork@protonmail.com>
2018-12-06 14:42:34 +0000

    UserDetailsSerializer for displaying points on logon

--- country_guessr/django_rest/api/serializers.py
+++ country_guessr/django_rest/api/serializers.py
@@ -1,5 +1,7 @@
 from world.models import WorldBorder
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 
 class WorldRandomCountrySerializer(GeoFeatureModelSerializer):
@@ -16,3 +18,13 @@ class WorldCountriesSerializer(GeoFeatureModelSerializer):
         model = WorldBorder
         fields = ('name', 'name', 'lon', 'lat', 'mpoly')
         geo_field = "mpoly"
+
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    """
+    django_rest_auth User model w/o password
+    """
+    class Meta:
+        model = get_user_model()
+        fields = ('pk', 'username', 'email', 'first_name', 'last_name', 'points')
+        read_only_fields = ('email', )
