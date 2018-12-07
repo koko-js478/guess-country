29de0b0bb4ab9d4a5f3231fe8a3b3e512ef665bf
softengineer123 <engineerwork@protonmail.com>
2018-12-07 16:23:17 +0000

    backend provider globals

--- country_guessr/ionic_app/src/providers/backend/backend.ts
+++ country_guessr/ionic_app/src/providers/backend/backend.ts
@@ -9,6 +9,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class BackendProvider {
 
+  url: string = "http://localhost:8000/";
+
+  auth_url: string = this.url + "api-token-auth/";
+  login_url: string = this.auth_url + "login/";
+  logout_url: string = this.auth_url + "logout/";
+  register_url: string = this.url + "register/";
+
+  world_url: string = this.url + "world/";
+
+  verify_url: string = this.url + "verify/";
+  refresh_url: string = this.url + "refresh/";
+
+
   constructor() {
   }
 
