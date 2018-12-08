77f711f609aefd9377118e83451fdda8a5e0a2f2
softengineer123 <engineerwork@protonmail.com>
2018-12-08 16:11:35 +0000

    logout settings functionality

--- country_guessr/ionic_app/src/pages/settings/settings.ts
+++ country_guessr/ionic_app/src/pages/settings/settings.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Alert, AlertController, App } from 'ionic-angular';
 import { UserProvider } from './../../providers/user/user';
+import { HttpClient } from '@angular/common/http';
+import { BackendProvider } from './../../providers/backend/backend';
 
 /**
  * Generated class for the SettingsPage page.
@@ -17,7 +19,32 @@ import { UserProvider } from './../../providers/user/user';
 export class SettingsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public user: UserProvider) {
+              public user: UserProvider,
+              private app: App,
+              private api: BackendProvider,
+              private alertCtrl: AlertController,
+              private http: HttpClient) {
+  }
+
+  logoutButtonAction() {
+    this.makeLogoutRequest().then(data => {
+      this.app.navPop();
+    });
+  }
+
+  makeLogoutRequest() {
+    return new Promise(resolve => {
+      this.http.post(this.api.logout_url, null).subscribe(data => {
+        resolve(data);
+      }, err => {
+        let alert = this.alertCtrl.create({
+          title: 'Unable to logout',
+          subTitle: 'This could be due to server issues.',
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      });
+    });
   }
 
 }
