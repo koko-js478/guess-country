7fe0ab64fe4aa4f85011783ba07fa1dd2a02a6e5
softengineer123 <engineerwork@protonmail.com>
2018-12-08 16:12:21 +0000

    remove declarations and import modules

--- country_guessr/ionic_app/src/app/app.module.ts
+++ country_guessr/ionic_app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SettingsPageModule } from './../pages/settings/settings.module';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
@@ -8,6 +9,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { MapPage } from './../pages/map/map';
 import { SettingsPage } from './../pages/settings/settings';
 import { LoginPage } from './../pages/login/login';
+import { LoginPageModule } from './../pages/login/login.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,12 +21,12 @@ import { UserProvider } from '../providers/user/user';
     MyApp,
     TabsPage,
     MapPage,
-    SettingsPage,
-    LoginPage,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    LoginPageModule,
+    SettingsPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
