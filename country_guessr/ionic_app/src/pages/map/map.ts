50a04dd5252621d18fcdede355242158f5fba2e9
softengineer123 <engineerwork@protonmail.com>
2018-12-09 20:59:23 +0000

    imports

--- country_guessr/ionic_app/src/pages/map/map.ts
+++ country_guessr/ionic_app/src/pages/map/map.ts
@@ -1,6 +1,9 @@
+import { BackendProvider } from './../../providers/backend/backend';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { UserProvider } from './../../providers/user/user';
+import leaflet from "leaflet";
 
 /**
  * Generated class for the MapPage page.
