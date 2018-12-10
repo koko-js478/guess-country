b4a309afdb8b242fa271d9d17050c414d96de6fe
softengineer123 <engineerwork@protonmail.com>
2018-12-10 21:58:08 +0000

    increment points endpoints and imports

--- country_guessr/django_rest/api/views.py
+++ country_guessr/django_rest/api/views.py
@@ -2,11 +2,17 @@ from random import randint
 
 from world.models import WorldBorder
 from rest_framework import viewsets
-from api.serializers import WorldRandomCountrySerializer, WorldCountriesSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from api.serializers import WorldCountriesSerializer
 from rest_framework.decorators import action
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404
+from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
 
+import json
 
 class WorldCountriesViewSet(viewsets.ReadOnlyModelViewSet):
     """Returns world countries
@@ -34,3 +40,19 @@ class WorldCountriesViewSet(viewsets.ReadOnlyModelViewSet):
 
         serializer = self.get_serializer(random_country, many=True)
         return HttpResponse(serializer.data)
+
+@api_view(['PATCH'])
+@permission_classes((AllowAny, ))
+def increment_points(request):
+    #   Try to load JSON if token passed as JSON, else assume entire body is token
+    try:
+        data = json.loads(request.body)
+        valid_data = VerifyJSONWebTokenSerializer().validate(data)
+        user = valid_data['user']
+    except (TypeError, json.JSONDecodeError):
+        return Response({"detail": "Invalid token."}, status.HTTP_400_BAD_REQUEST)
+
+    user.points += 1
+    user.save()
+
+    return Response({"detail": "Successfully incremented."}, status.HTTP_200_OK)
